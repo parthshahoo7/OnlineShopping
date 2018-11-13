ddfb8d0e8adcca98dbd2f680d77469e443e0bc0d
parthshahoo7 <parthnileshshah@gmail.com>
2018-11-13 17:09:36 -0500

    aasignment 7//default

---
+++ SpringMvc/src/main/java/edu/shah/model/Location.java
@@ -0,0 +1,19 @@
+package edu.shah.model;
+
+public class Location {
+	private String location;
+
+	public Location(String location) {
+		super();
+		this.location = location;
+	}
+
+	public String getLocation() {
+		return location;
+	}
+
+	public void setLocation(String location) {
+		this.location = location;
+	}
+
+}
